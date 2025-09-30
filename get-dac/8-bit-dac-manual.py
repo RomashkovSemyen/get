55b80b35e459e5c985693a7ae97c001ee3925b88
Romashkov Semyen <romashkovsemen68@gmail.com>
2025-09-30 19:07:48 +0300

    r2r

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -5,7 +5,7 @@ dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(dac_bits, GPIO.OUT)
 GPIO.output(dac_bits, 0)
 
-dynamic_range = 3.1
+dynamic_range = 3.11
 
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
@@ -13,7 +13,7 @@ def voltage_to_number(voltage):
         print ("Устанавливаем 0.0 В")
         return 0
     
-    return int(voltage / dynamic_range * 255)
+    return int(voltage / (dynamic_range) * 255)
 
 def number_to_dac(value):
     GPIO.output(dac_bits, [int(element) for element in bin(value)[2:].zfill(8)])
---
+++ get-dac/r2r_dac.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+class r2r_dac:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+    def set_number(self, number, dynamic_range):
+        if not (0.0 <= voltage <= dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} B")
+            print ("Устанавливаем 0.0 В")
+            return 0
+        return int(voltage / (dynamic_range) * 255)
+    def set_voltage(self, voltage, gpio_bits):
+        GPIO.output(gpio_bits, [int(element) for element in bin(voltage)[2:].zfill(8)])
+
+
+
+if __name__ == "__main__":
+    dac = r2r_dac([16, 20, 21, 25, 26, 17, 27, 22], 3.11, True)
+    try:
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                number = dac.set_number(voltage, 3.11)
+                dac.set_voltage(number, [16, 20, 21, 25, 26, 17, 27, 22])
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз\n")
+    
+
+    finally:
+        dac.deinit()
